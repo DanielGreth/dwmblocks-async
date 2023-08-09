35edd27e1c60831927514325364a4148ff170f3f
DanielGreth <dgsoerensen@protonmail.com>
2023-08-09 19:02:28 +0200

    statusbar

--- config.c
+++ config.c
@@ -4,16 +4,23 @@
 #include "util.h"
 
 Block blocks[] = {
-    {"sb-mail",    600,  1 },
-    {"sb-music",   0,    2 },
-    {"sb-disk",    1800, 3 },
+//    {"sb-mail",    600,  1 },
+//    {"sb-music",   0,    2 },
+//    {"sb-disk",    1800, 3 },
+//    {"sb-memory",  10,   4 },
+//    {"sb-loadavg", 5,    5 },
+//    {"sb-mic",     0,    6 },
+//    {"sb-record",  0,    7 },
+    {"sb-updates", 1800, 1 },
+    {"sb-network", 10,   2 },
+    {"sb-up_down", 3,    3 },
     {"sb-memory",  10,   4 },
-    {"sb-loadavg", 5,    5 },
-    {"sb-mic",     0,    6 },
-    {"sb-record",  0,    7 },
-    {"sb-volume",  0,    8 },
-    {"sb-battery", 5,    9 },
-    {"sb-date",    1,    10},
+    {"sb-cpu",     5,    5 },
+    {"sb-disk",    1800, 6 },
+    {"sb-volume",  5,    7 },
+    {"sb-battery", 10,   8 },
+    {"sb-time",    1,    9 },
+    {"sb-date",    1800, 10},
 };
 
 const unsigned short blockCount = LEN(blocks);
